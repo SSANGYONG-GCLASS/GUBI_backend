55f01bb1dc2b168945416dbd0f0ddfd53e61b69a
gyu9907 <gyu9907@gmail.com>
2025-05-10 16:09:59 +0900

    feat: 장바구니 CRUD 구현

--- src/main/java/com/spring/gubi/dto/carts/GetCartsRequest.java
+++ src/main/java/com/spring/gubi/dto/carts/GetCartsRequest.java
@@ -10,8 +10,8 @@ import org.springframework.data.domain.Sort;
 @Setter
 public class GetCartsRequest {
     private Long userNo; // 회원 일련번호, TODO: 나중에 토큰 또는 세션에서 가져오게 되면 삭제할 것
-    private int page;
-    private int size;
+    private int page = 1;
+    private int size = 10;
 
     public Pageable getPageable() {
         return PageRequest.of(page-1, size, Sort.Direction.DESC, "id");
