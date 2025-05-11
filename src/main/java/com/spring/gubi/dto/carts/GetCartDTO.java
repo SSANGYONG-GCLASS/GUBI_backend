75c414b13ae845ac8a90a40962c818f61ab94bbb
gyu9907 <gyu9907@gmail.com>
2025-05-11 17:06:06 +0900

    chore: 장바구니 DTO 이름 변경, 장바구니 수량 수정 시 회원 정보 확인

---
+++ src/main/java/com/spring/gubi/dto/carts/GetCartDTO.java
@@ -0,0 +1,43 @@
+package com.spring.gubi.dto.carts;
+
+import com.spring.gubi.domain.carts.Cart;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetCartDTO {
+    private Long cartNo;
+    private Integer cartCnt; // 수량
+
+    // 상품
+    private Long productNo;
+    private String productName;
+    private String productThumbnailImg;
+    private Integer productPrice;
+    private Integer productDeliveryPrice;
+    private Integer ProductPointPct;
+
+    // 상품 옵션
+    private Long optionNo;
+    private String optionName;
+    private String optionColor;
+    private String optionImg;
+
+    public GetCartDTO(Cart cart) {
+        this.cartNo = cart.getId();
+        this.cartCnt = cart.getCnt();
+
+        this.productNo = cart.getOption().getProduct().getId();
+        this.productName = cart.getOption().getProduct().getName();
+        this.productThumbnailImg = cart.getOption().getProduct().getThumbnail_img();
+        this.productPrice = cart.getOption().getProduct().getPrice();
+        this.productDeliveryPrice = cart.getOption().getProduct().getDelivery_price();
+        this.ProductPointPct = cart.getOption().getProduct().getPoint_pct();
+
+        this.optionNo = cart.getOption().getId();
+        this.optionName = cart.getOption().getName();
+        this.optionColor = cart.getOption().getColor();
+        this.optionImg = cart.getOption().getImg();
+    }
+}
--- src/main/java/com/spring/gubi/repository/carts/CartRepository.java
+++ src/main/java/com/spring/gubi/repository/carts/CartRepository.java
@@ -1,14 +1,13 @@
 package com.spring.gubi.repository.carts;
 
 import com.spring.gubi.domain.carts.Cart;
-import com.spring.gubi.domain.product.Option;
-import com.spring.gubi.domain.users.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +17,10 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.user.id = :userNo AND c.option.id = :optionNo")
     Optional<Cart> findByUser_IdAndOption_Id(Long userNo, Long optionNo);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.option o JOIN FETCH o.product p WHERE c.id IN :cartNoList AND c.user.id = :userNo")
+    List<Cart> findByIdInAndUser_Id(List<Long> cartNoList, Long userNo);
+
+    @Query("SELECT c FROM Cart c WHERE c.user.id = :userNo")
+    Optional<Cart> findByIdAndUser_Id(Long id, Long userNo);
 }
